b00512eb03f5b553be4482b4698b3478e4a023ed
Sayandip Halder <sayandiph4@gmail.com>
2022-01-21 03:35:33 +0530

    display the data from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect( () => {
+    axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    ).then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    }).catch(err => console.error(err));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter( coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return(
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" className="coin-input" placeholder="Search" onChange={handleChange}/>
+        </form>
+      </div>
+      {
+        filteredCoins.map(coin => (
+          <Coin 
+            key={coin.id}
+            image={coin.image}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.total_volume}
+          />
+        ))
+      }
     </div>
   );
 }
